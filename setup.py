84204421bc4300aa06849926b89dd6a9c8ab2bb4
a1025 <a1025952421@outlook.com>
2021-12-12 11:36:38 +0800

    split setup info

--- setup.py
+++ setup.py
@@ -1,25 +1,19 @@
 import glob
 
 from setuptools import setup
+
 # Available at setup time due to pyproject.toml
 try:
     from pybind11.setup_helpers import Pybind11Extension as Extension
 except ImportError:
     from setuptools import Extension
 
-__version__ = "1.2.2"
+__version__ = "1.3.0"
 
 setup(
-    name="pysilk-mod",
     version=__version__,
-    author="DCZYewen",
-    author_email="contact@basicws.net",
-    url="https://github.com/DCZYewen/Python-Silk-Module",
-    description="Python silk decode/encoder",
-    long_description="Python silk decode/encoder bindings using pybind11",
-    packages=["src/pysilk"],
-    requires=["pybind11"],
-    zip_safe=False,
+    install_requires=["pybind11"],
+    zip_safe=True,
     ext_modules=[
         Extension(
             "_pysilk", ["src/silk/_pysilk.cpp", "src/silk/codec.cpp", *glob.glob("src/silk/src/*.c")],
